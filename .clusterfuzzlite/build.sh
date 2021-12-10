4d7cc04305a2be2a1bf26df138cbabfcdea1577e
Winifred Paul <winifred.paul@partner.bmwgroup.de>
2021-12-10 18:39:46 +0100

    Fix build.sh based on grpc, run local tests

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -11,3 +11,44 @@
 # $CXX $CXXFLAGS -std=c++11 -Iinclude \
 #     /path/to/name_of_fuzzer.cc -o $OUT/name_of_fuzzer \
 #     $LIB_FUZZING_ENGINE /path/to/library.a
+
+# build grpc
+# Temporary hack, see https://github.com/google/oss-fuzz/issues/383
+readonly NO_VPTR='--copt=-fno-sanitize=vptr --linkopt=-fno-sanitize=vptr'
+
+# Copied from envoy's build.sh
+# Copy $CFLAGS and $CXXFLAGS into Bazel command-line flags, for both
+# compilation and linking.
+#
+# Some flags, such as `-stdlib=libc++`, generate warnings if used on a C source
+# file. Since the build runs with `-Werror` this will cause it to break, so we
+# use `--conlyopt` and `--cxxopt` instead of `--copt`.
+#
+readonly EXTRA_BAZEL_FLAGS="$(
+for f in ${CFLAGS}; do
+  echo "--conlyopt=${f}" "--linkopt=${f}"
+done
+for f in ${CXXFLAGS}; do
+  echo "--cxxopt=${f}" "--linkopt=${f}"
+done
+if [ "$SANITIZER" = "undefined" ]
+then
+  # Bazel uses clang to link binary, which does not link clang_rt ubsan library for C++ automatically.
+  # See issue: https://github.com/bazelbuild/bazel/issues/8777
+  echo "--linkopt=$(find $(llvm-config --libdir) -name libclang_rt.ubsan_standalone_cxx-x86_64.a | head -1)"
+fi
+)"
+
+bazel build --config=asan-libfuzzer --dynamic_mode=off \
+  --spawn_strategy=standalone \
+  --genrule_strategy=standalone \
+  ${NO_VPTR} \
+  --strip=never \
+  --linkopt=-lc++ \
+  --linkopt=-pthread \
+  --copt=${LIB_FUZZING_ENGINE} \
+  --linkopt=${LIB_FUZZING_ENGINE} \
+  ${EXTRA_BAZEL_FLAGS} \
+  //:fuzz_test_run
+
+cp bazel-bin/fuzz_test_run.runfiles/__main__/fuzz_test_bin "$OUT/"
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # clusterfuzzlite_int
-Try out clusterfuzzlite
+Try out clusterfuzzlite - here are the steps that were followed
 
 ## A minimal fuzz test
 This minimal example is from the libfuzzer documentation
@@ -10,3 +10,19 @@ Bazel build uses [bazel fuzzing rules](https://github.com/bazelbuild/rules_fuzzi
 $ bazel run --config=asan-libfuzzer //:fuzz_test_run
 ```
 
+## Adding clusterfuzzlite support
+Follow the documentation at https://google.github.io/clusterfuzzlite/build-integration/
+And make sure to do the local tests as per documentation
+
+Adopt build.sh based on grpc project's build.sh to get to a working set of compiler options. Was a quick hack to make things work.
+
+In this particular setup the following tests were done
+```sh
+python infra/helper.py build_image --external $PATH_TO_PROJECT
+python infra/helper.py build_fuzzers --external $PATH_TO_PROJECT --sanitizer address
+python infra/helper.py check_build --external $PATH_TO_PROJECT --sanitizer address
+python infra/helper.py run_fuzzer --external $PATH_TO_PROJECT fuzz_test_bin
+python infra/helper.py build_fuzzers --external --sanitizer coverage $PATH_TO_PROJECT
+python infra/helper.py check_build --external --sanitizer coverage $PATH_TO_PROJECT
+python infra/helper.py run_fuzzer --external --sanitizer coverage $PATH_TO_PROJECT fuzz_test_bin
+```
